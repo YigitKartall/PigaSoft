3a5306d10742589c9da7eaed373f2c6b6c37c9a1
Mustafa Yigit Kartal <mustafakartal680@gmail.com>
2025-10-09 17:57:22 +0300

    Add files via upload

---
+++ Js-first.js
@@ -0,0 +1,7 @@
+var number1= 10;
+var number2= 20;
+
+console.log("The sum of two number: " , number1+number2);
+document.writeln("The sum of two number: " , number1+number2);
+alert("The sum of two number: " , number1+number2);
+
